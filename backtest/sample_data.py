41818a1dbdc8048bff78e826e380f6af24af3946
Ferenc <ferenc4.fazekas@gmail.com>
2020-03-23 23:35:33 +1100

    added plot for rc

--- backtest/sample_data.py
+++ backtest/sample_data.py
@@ -9,11 +9,16 @@ ONE_YEAR = 365
 
 def sample_growth_data(days=ONE_YEAR):
     random.seed(1)
-    current_val = 100.0
+    ccy1_val = 100.0
+    ccy2_val = 100.0
     rates = []
     dt = datetime(2000, 1, 1)
+    ccy1 = Ccy("sample1", "")
+    ccy2 = Ccy("sample2", "")
     for i in range(0, days):
-        rates.append(Rate(Ccy("sample1", "sample1"), AUD, dt, current_val))
-        current_val += current_val * random.randint(10, 40) / float(100)
+        rates.append(Rate(ccy1, AUD, dt, ccy1_val))
+        rates.append(Rate(ccy2, AUD, dt, ccy2_val))
+        ccy1_val += ccy1_val * random.randint(-32, 40) / float(100)
+        ccy2_val += ccy2_val * random.randint(-32, 40) / float(100)
         dt += timedelta(days=1)
     return rates
